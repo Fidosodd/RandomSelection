660e686f31c657bf5886b0e12e3d87f41ae65848
Colin Hale-Brown <colinhalebrown+GIT@gmail.com>
2018-09-11 15:59:24 -0700

    fix the manifest and the background.js

--- Script.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-document.getElementById("mybutton").onclick = quadFunction;
-}
-
-var classSize = 20
-var randomChoice = Math.floor(Math.random() * classSize); 
-document.write(randomChoice)
---
+++ background.js.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+document.getElementById("mybutton").onclick = quadFunction;
+}
+
+var classSize = 20
+var randomChoice = Math.floor(Math.random() * classSize); 
+document.write(randomChoice)
