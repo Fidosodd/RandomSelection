f578d3e9b971501630d02f0aad8dbd91a5f3c056
Hunter Key <hunterjkey@gmail.com>
2018-09-14 14:33:45 -0700

    Add files via upload

--- background.js
+++ background.js
@@ -1,8 +1,33 @@
 window.onload = function() {
 document.getElementById("mybutton").onclick = randomSelector;
 }
+
 function randomSelector() {
-	var classSize = 20
-	var randomChoice = Math.floor(Math.random() * classSize); 
-	document.write(randomChoice)
+	
+	{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", "test.txt", false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                /*document.write(allText);*/
+				var splitString = allText.split(" ");
+				Length = splitString.length;
+				/*var test = allText.slice(2, 8);*/
+				/*document.write(splitString);*/
+				
+				var classSize = Length
+				var randomChoice = Math.floor(Math.random() * classSize); 
+				/*document.write(randomChoice)*/
+				document.write(splitString[randomChoice])
+				
+            }
+        }
+    }
+    rawFile.send(null);
+}
   }
