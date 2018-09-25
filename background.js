868bad6640406bf24ce77f32ecae453c2f73079a
Colin Hale-Brown <colinhalebrown+GIT@gmail.com>
2018-09-25 09:32:26 -0700

    closer

--- background.js
+++ background.js
@@ -67,5 +67,16 @@ function randomSelector() {
 }
   }
   function storageEditor {
-	  
+	// Get a value saved in a form.
+    var theValue = textarea.listContent;
+    // Check that there's some code there.
+    if (!theValue) {
+      message('Error: No value specified');
+      return;
+    }
+    // Save it using the Chrome extension storage API.
+    chrome.storage.sync.set({'value': theValue}, function() {
+      // Notify that we saved.
+      message('Settings saved');
+    });
   }
