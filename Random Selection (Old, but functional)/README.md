fdec27933fc103f3c77e1370a6cffcd6108939f6
Morgan Thomas <33166036+Fidosodd@users.noreply.github.com>
2018-09-25 07:36:18 -0700

    Add files via upload

---
+++ Random Selection (Old, but functional)/README.md
@@ -0,0 +1,20 @@
+# Student Selector
+Chrome extension that selects a random student from a text document stored locally on the client's computer. 
+
+This friendly sorter will make chooseing a student easy. We know other software can be scary so we designed the RandomSelection app (we are working on the name) to be as friendly as possible. 
+# 8 Preloaded Example lists:
+1. Example Class
+2. 50 States
+3. Companies
+4. Ferns
+5. Programming Languages
+6. Animals
+7. Batteries
+8. Your Grade
+
+# Attributions:
+
+https://stackoverflow.com/questions/18933085/display-text-file-in-javascript
+https://www.digitalocean.com/community/tutorials/how-to-index-split-and-manipulate-strings-in-javascript
+https://stackoverflow.com/questions/2858121/convert-comma-separated-string-to-array
+https://www.w3schools.com 
---
+++ Random Selection (Old, but functional)/background.js
@@ -0,0 +1,34 @@
+window.onload = function() {
+document.getElementById("mybutton").onclick = randomSelector;
+}
+
+function randomSelector() {
+	
+	{
+    var rawFile = new XMLHttpRequest();
+	var filename = document.getElementById("list").value;
+    rawFile.open("GET", filename, true);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                /*document.write(allText);*/
+				var splitString = allText.split(" ");
+				Length = splitString.length;
+				/*var test = allText.slice(2, 8);*/
+				/*document.write(splitString);*/
+				
+				var classSize = Length
+				var randomChoice = Math.floor(Math.random() * classSize); 
+				/*document.write(randomChoice)*/
+				document.getElementById("result").innerHTML = (splitString[randomChoice])
+				
+            }
+        }
+    }
+    rawFile.send(null);
+}
+  }
