4c0daa0765930f821af675448530dd51d8e38956
Colin Hale-Brown <colinhalebrown+git@gmail.com>
2018-09-25 21:55:25 -0700

    sleep

--- storage.js
+++ storage.js
@@ -1,3 +1,5 @@
+var periodChange = 0;
+
 window.onload = function() {
 	document.getElementById("editButton").onclick = storageEditor;
   document.getElementById("saveButton").onclick = saveEdits;
@@ -5,17 +7,27 @@ window.onload = function() {
   function storageEditor() {
     var listname = document.getElementById("listSelect").value;
     console.log('Value currently is ' + listname);
-    chrome.storage.local.get(['listname'], function(result) {
-          console.log('Value currently is ' + result.listname);
+    chrome.storage.local.get(['listname'], function(data) {
+          console.log('Value currently is ' + data.listname);
+          document.getElementById('listContent').textContent = data.listname;
         });
-    document.getElementById('listContent').textcontent = (splitString[result.listname]);
+    let port = chrome.extension.connect({
+      name: "Load Student Names"
+    });
+
+    port.postMessage(listname);
+    port.onMessage.addListener(function(msg) {
+      console.log("message received");
+		
+      console.log(msg);
+    });
   }
   function saveEdits() {
     var listitems = document.getElementById("listContent").value;
     var listname = document.getElementById("listSelect").value;
     console.log('Value currently is ' + listitems);
     console.log('Value currently is ' + listname);
-    chrome.storage.local.set({listname: listitems}, function(result) {
-          console.log('Value currently is ' + result.listname);
+    chrome.storage.local.set({'listname': listitems}, function(data) {
+          console.log('Value currently is ' + data.listname);
         });
   }
